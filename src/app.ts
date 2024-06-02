cef6967607be4a01b505e3077ff9c8be4247f777
Gooner91 <hassan91ahmed@gmail.com>
2024-06-02 20:09:48 +0200

    Moved app.ts under src

--- src/app.ts
+++ src/app.ts
@@ -6,15 +6,15 @@ dotenv.config();
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true,
+  socketMode: false, // setting for now to false (no app token provided atm)
 });
 
 (async () => {
   try{
-    await app.start(process.env.port || 3000);
+    await app.start(process.env.PORT || 3000);
     console.log("estim8 running via Bolt");
   }
   catch (error){
     console.error("unable to start app", error);
   }
-})
+})();

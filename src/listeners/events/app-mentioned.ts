32b4b31b574c20f644f6671e1c550d459731f69f
Gooner91 <hassan91ahmed@gmail.com>
2024-06-30 20:32:45 +0200

    fixed linter violations

--- src/listeners/events/app-mentioned.ts
+++ src/listeners/events/app-mentioned.ts
@@ -1,28 +1,32 @@
 import { AllMiddlewareArgs, SlackEventMiddlewareArgs } from "@slack/bolt";
 
-const appMentionCallback = async ({event, context, client, say}: AllMiddlewareArgs & SlackEventMiddlewareArgs<'app_mention'>) => {
-  try{
-    await say({"blocks": [
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": `Thanks for the mention <@${event.user}>! Here is a button`
-        },
-        "accessory": {
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": "button",
-            "emoji": true,
+const appMentionCallback = async ({
+  event,
+  say,
+}: AllMiddlewareArgs & SlackEventMiddlewareArgs<"app_mention">) => {
+  try {
+    await say({
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `Thanks for the mention <@${event.user}>! Here is a button`,
           },
-          "value": "click_me_123",
-          "action_id": "first_button",
-        }
-      }
-    ]});
-  }
-  catch(error){
+          accessory: {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "button",
+              emoji: true,
+            },
+            value: "click_me_123",
+            action_id: "first_button",
+          },
+        },
+      ],
+    });
+  } catch (error) {
     console.error(error);
   }
 };
